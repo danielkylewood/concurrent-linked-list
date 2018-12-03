446ee03f83618d5d8d70722b6743985d6f001ed4
danielkylewood <danielkylewood@gmail.com>
2018-12-03 16:23:57 +0000

    Remove unused usings

--- src/ConcurrentLinkedList/ConcurrentLinkedList.cs
+++ src/ConcurrentLinkedList/ConcurrentLinkedList.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Concurrent;
-using System.Runtime.CompilerServices;
 using System.Threading;
 
 namespace ConcurrentLinkedList
--- src/ConcurrentLinkedList/IConcurrentLinkedList.cs
+++ src/ConcurrentLinkedList/IConcurrentLinkedList.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ConcurrentLinkedList
+﻿namespace ConcurrentLinkedList
 {
     public interface IConcurrentLinkedList<T>
     {
